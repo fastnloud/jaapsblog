cf78cf2e19f8080a476be4af0fcd37688c07df45
jaap <jaap.vanturnhout@gmail.com>
2013-04-11 23:51:04 +0200

    Updated Footer links and added a simple popup handler.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -44,3 +44,10 @@ function loadSearch(){
         });
     }
 }
+
+function popup(url, title, w, h) {
+    var left = (screen.width/2)-(w/2),
+        top = (screen.height/2)-(h/2);
+
+    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
