ce11b687e3acbfcd2d2298b8bb95a207d498a188
jaap <jaap.vanturnhout@gmail.com>
2013-04-19 15:30:26 +0200

    No comment.

--- module/Blog/src/Blog/Navigation/BlogNavigationFactory.php
+++ module/Blog/src/Blog/Navigation/BlogNavigationFactory.php
@@ -9,7 +9,6 @@ class BlogNavigationFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $navigation = new BlogNavigation();
-
         return $navigation->createService($serviceLocator);
     }
 }
