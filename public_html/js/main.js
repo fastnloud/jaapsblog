52e6add09d7477302ee174e49fc1ff5a7935ac13
jaap <jaap.vanturnhout@gmail.com>
2013-08-10 20:13:42 +0200

    Re-styled search box/ event.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
+    // global variable
+    bodyWidth = $('body').width();
+
     loadSearch();
-    replyForm();
+    validateReplyForm();
 });
 
-function replyForm(){
+$(window).resize(function() {
+    // reload if body width changes
+    if (bodyWidth != $('body').width()) {
+        // register new size
+        bodyWidth = $('body').width();
+
+        // reset
+        loadSearchCss();
+    }
+});
+
+function validateReplyForm(){
     var form = $('#reply-form');
 
     if (form) {
         $(form).find('input[type=text], textarea').keyup(function() {
+            // validate input
             if ('' == $(this).val()) {
                 $(this).addClass('error');
             } else {
@@ -23,6 +38,9 @@ function loadSearch(){
         searchInput      = $('#search input');
 
     if (searchContainer) {
+        // apply css props
+        loadSearchCss();
+
         $(searchInput).focus(function() {
             if ('Search...' == $(this).val()) {
                 $(this).val('');
@@ -41,20 +59,63 @@ function loadSearch(){
     }
 }
 
+function loadSearchCss() {
+    var searchContainer = $('#search');
+
+    if (searchContainer) {
+        $(searchContainer).height($('#page').height()+40);
+        $(searchContainer).css('position', 'fixed');
+        $(searchContainer).css('opacity', 0);
+
+        resetTransition(searchContainer);
+
+        // for desktop version
+        if (bodyWidth > 800) {
+            $(searchContainer).height($('#page').height()+20);
+            $(searchContainer).css('margin', '0 -640px 0 0');
+            $(searchContainer).css('top', '86px');
+            $(searchContainer).css('right', '50%');
+        } else { // reset if needed
+            $(searchContainer).height($('#page').height()+40);
+            $(searchContainer).css('margin', '54px -230px 0 0');
+            $(searchContainer).css('top', '0');
+            $(searchContainer).css('right', '0');
+        }
+    }
+}
+
 function search(searchContainer) {
     if (0 == searchContainer.css('opacity')) {
         $(searchContainer).transition({
-            y: 170,
-            opacity: 1,
+            x: -230,
+            opacity: 0.9,
             delay: 0
+        }, function() {
+            $(this).css('position', 'absolute');
+
+            if (bodyWidth > 800) {
+                $(this).height($('#page').height()+20);
+            } else { // reset if needed
+                $(this).height($('#page').height()+40);
+            }
         });
     } else {
+        // from the start
+        $(searchContainer).css('position', 'fixed');
         $(searchContainer).transition({
-            y: 0,
+            x: 230,
             opacity: 0,
             delay: 0
         });
     }
+}
+
+function resetTransition(elm) {
+    $(elm).css('-webkit-transform', 'none');
+    $(elm).css('-moz-transform', 'none');
+    $(elm).css('-ms-transform', 'none');
+    $(elm).css('-o-transform', 'none');
+    $(elm).css('transform', 'none');
 }
 
 function popup(url, title, w, h) {
