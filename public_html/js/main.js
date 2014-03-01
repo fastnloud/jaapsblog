3c582be0e5487f566d6a9a24b6677b7cebb36777
jaap <jaap.vanturnhout@gmail.com>
2014-03-01 11:21:46 +0100

    Added jQuery / load event.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,16 @@
+// load events
+$(document).ready(function() {
+    // search input field
+    $('.search form > input').bind('focus focusout', function() {
+        if ('Search...' == $(this).val()) {
+            $(this).val('');
+        } else if ('' == $(this).val()) {
+            $(this).val('Search...');
+        }
+    });
+});
+
+// simple pupup handler
 function popup(url, title, w, h) {
     var left = (screen.width/2)-(w/2),
         top = (screen.height/2)-(h/2);
