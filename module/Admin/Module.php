8871a4ee4939263039254d974508a426793856de
jaap <thinkfast22>
2015-01-24 19:16:53 +0100

    Clean-up Module file.

--- module/Admin/Module.php
+++ module/Admin/Module.php
@@ -2,14 +2,6 @@
 
 namespace Admin;
 
-use Admin\Controller\BlogController;
-use Admin\Controller\CategoryController;
-use Admin\Controller\PageController;
-use Admin\Controller\ReplyController;
-use Admin\Controller\SiteController;
-use Admin\Controller\StatusController;
-use Zend\Mvc\Controller\ControllerManager;
-
 /**
  * Class Module
  * @package Admin
@@ -39,53 +31,6 @@ class Module
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
-        );
-    }
-
-    /**
-     * @return array
-     */
-    public function getControllerConfig()
-    {
-        return array(
-            'factories' => array(
-                /*'Admin\Controller\Page' => function(ControllerManager $cm) {
-                    $controller = new PageController();
-                    $controller->setPageService($cm->getServiceLocator()->get('PageService'));
-
-                    return $controller;
-                },
-                'Admin\Controller\Blog' => function(ControllerManager $cm) {
-                    $controller = new BlogController();
-                    $controller->setBlogService($cm->getServiceLocator()->get('BlogService'));
-
-                    return $controller;
-                },
-                'Admin\Controller\Site' => function(ControllerManager $cm) {
-                    $controller = new SiteController();
-                    $controller->setSiteService($cm->getServiceLocator()->get('SiteService'));
-
-                    return $controller;
-                },
-                'Admin\Controller\Reply' => function(ControllerManager $cm) {
-                    $controller = new ReplyController();
-                    $controller->setReplyService($cm->getServiceLocator()->get('ReplyService'));
-
-                    return $controller;
-                },
-                'Admin\Controller\Status' => function(ControllerManager $cm) {
-                    $controller = new StatusController();
-                    $controller->setStatusService($cm->getServiceLocator()->get('StatusService'));
-
-                    return $controller;
-                },
-                'Admin\Controller\Category' => function(ControllerManager $cm) {
-                    $controller = new CategoryController();
-                    $controller->setCategoryService($cm->getServiceLocator()->get('CategoryService'));
-
-                    return $controller;
-                }*/
-            )
         );
     }
 
