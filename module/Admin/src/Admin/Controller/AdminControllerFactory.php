c31e1e0c3e813621111c24ceea7b74de5badbc96
jaap <thinkfast22>
2015-02-10 20:54:55 +0100

    Added PHP docs.

--- module/Admin/src/Admin/Controller/AdminControllerFactory.php
+++ module/Admin/src/Admin/Controller/AdminControllerFactory.php
@@ -5,9 +5,17 @@ namespace Admin\Controller;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
+/**
+ * Class AdminControllerFactory
+ * @package Admin\Controller
+ */
 class AdminControllerFactory implements FactoryInterface
 {
 
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return AdminController
+     */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $config = $serviceLocator->getServiceLocator()
