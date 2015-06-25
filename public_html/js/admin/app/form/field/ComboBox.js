0e9887b292c7399fc51d44a5fad918dddb0fb151
jaap <thinkfast22>
2015-06-25 22:35:21 +0200

    Set forceSelection = true by default.

--- public_html/js/admin/app/form/field/ComboBox.js
+++ public_html/js/admin/app/form/field/ComboBox.js
@@ -1,11 +1,11 @@
 Ext.define('App.form.field.ComboBox', {
-    override : 'Ext.form.field.ComboBox',
+    override       : 'Ext.form.field.ComboBox',
+    forceSelection : true,
 
     listeners : {
         change : function(combobox) {
             if (Ext.isObject(combobox.getValue())) {
                 combobox.setValue(combobox.getValue().id);
-                combobox.forceSelection = true;
             }
         }
     }
